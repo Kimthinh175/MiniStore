1f7df53aa7954df642fc7e1e4af406bb6f2c9f77
Kimthinh175 <kimthinhtran175@gmail.com>
2025-06-19 10:41:59 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+$('#banner-list').owlCarousel({
+    loop:true,
+    margin:30,
+	nav:true,
+    dots:false,
+	autoplay:false,
+	smartSpeed:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
